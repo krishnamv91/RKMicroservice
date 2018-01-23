62401dca4bda4e55083bb87a2a5e7fb667264cdd
Radhakrishna <krishnamv91@gmail.com>
2018-01-23 16:11:52 +0530

    my new changes

--- MicroserviceRk.java
+++ MicroserviceRk.java
@@ -3,6 +3,12 @@ public class MicroserviceRk
   public static void main(String[] args)
 	{
 		System.out.println("RK");
+
+		System.out.println("IMP ");
+
 	  	System.out.println("Hmmm");
+
+		System.out.println("Merge/Rebase");
+
 	}
 }
